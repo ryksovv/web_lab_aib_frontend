baed9e6f5d1bd94a534ece27022dae2d2745eecf
Ryksovv <remics246@gmail.com>
2023-12-06 23:04:02 +0300

    Update 10 lab

--- labs/Lab_10_REACT_Routing/solution/route/src/App.js
+++ labs/Lab_10_REACT_Routing/solution/route/src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 import { Routes, Route, Link, useParams } from 'react-router-dom'
-
 import { About } from './routes/About'
+import { useState,useEffect } from 'react';
 
 
 function App() {
-  const users = [
-    { id: 1, name: 'User 1', age: 25 },
-    { id: 2, name: 'User 2', age: 30 },
-    { id: 3, name: 'User 3', age: 24 }
-  ];
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://swapi.dev/api/people/');
+        const data = await response.json();
+        setPeople(data.results);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
@@ -18,22 +28,22 @@ function App() {
         <Link to="/about">About</Link>
       </header>
       <Routes>
-        <Route path="/" element={<UserList users={users} />} />
+        <Route path="/" element={<UserList people={people} />} />
         <Route path="/about" element={<About />} />
-        <Route path="/user/:id" element={<UserDetails users={users} />} />
+        <Route path="/user/:id" element={<UserDetails people={people} />} />
       </Routes>
     </>
   );
 }
 
-function UserList({ users }) {
+function UserList({ people }) {
   return (
     <div>
-      <h2>Users</h2>
+      <h2>People</h2>
       <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            <Link to={`/user/${user.id}`}>{user.name}</Link>
+        {people.map((person) => (
+          <li key={person.url}>
+            <Link to={`/user/${extractIdFromUrl(person.url)}`}>{person.name}</Link>
           </li>
         ))}
       </ul>
@@ -41,22 +51,27 @@ function UserList({ users }) {
   );
 }
 
-function UserDetails({ users }) {
-
+function UserDetails({ people }) {
   const { id } = useParams();
+  const person = people.find((p) => extractIdFromUrl(p.url) === id);
 
-  const user = users.find((u) => u.id === parseInt(id, 10));
-
-  if (!user) {
-    return <div>User not found</div>;
+  if (!person) {
+    return <div>Person not found</div>;
   }
 
   return (
     <div>
-      <h2>{user.name}</h2>
-      <p>Age: {user.age}</p>
+      <p>ID: {extractIdFromUrl(person.url)}</p>
+      <h2>{person.name}</h2>
+      <p>Birth Year: {person.birth_year}</p>
     </div>
   );
+}
+
+// Helper function to extract ID from SWAPI URL
+function extractIdFromUrl(url) {
+  const matches = url.match(/\/(\d+)\/$/);
+  return matches ? matches[1] : null;
 }
 
 export default App;
